2900d8ea223909fc91eeace8ecff2888a5a7da61
Marcin Lech <arcinech@gmail.com>
2022-07-23 17:22:03 +0200

    Add: concerts mongoose controller, model and use

---
+++ controllers/concerts.controller.js
@@ -0,0 +1,81 @@
+const Concert = require('../models/concert.model');
+
+exports.getAll = async (req, res) => { 
+  try { 
+    res.send(await Concert.find());
+  }
+  
+  catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.postConcert = async (req, res) => { 
+  const {performer, genre, price, day, image} = req.body;
+
+  try {
+    const exist = await Concert.findOne({performer, genre, price, day, image});
+
+    if(performer && genre && price && day && image && !exist) {
+      const newConcert = new Concert({performer: performer, genre: genre, price: price, day: day, image: image});
+      await newConcert.save();
+      res.json({ message: 'OK' });
+    } else if(exist){
+      res.status(409).json({message: 'This concert already exists on this day...'});
+    } else res.status(400).json({message: 'Error'});
+  }
+
+  catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.getById = async (req, res) => { 
+  try {
+    const concert = await Concert.findById(req.params.id);
+
+    if(concert) res.send(concert);
+    else res.status(404).json({message: 'Not found'});
+  }
+
+  catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.putById = async (req, res) => {
+  const {performer, genre, price, day, image} = req.body;
+
+  try {
+    const exist = await Concert.findOne({performer, genre, price, day, image});
+
+    if(exist) {
+      exist.performer = performer;
+      exist.genre = genre;
+      exist.price = price;
+      exist.day = day;
+      exist.image = image;
+      await exist.save();
+      res.json({ message: 'OK' });
+    } else res.status(404).json({ message: 'Not found...' });
+  }
+
+  catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.deleteById = async (req, res) => {
+  try {
+    const concert = await Concert.findById(req.params.id);
+
+    if(concert) {
+      await concert.remove();
+      res.json({ message: 'OK' });
+    } else res.status(404).json({ message: 'Not found...' });
+  }
+
+  catch (err) {
+    res.status(500).send(err);
+  }
+};
--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,42 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const {v4: uuidv4} = require('uuid');
+const ConcertsController = require('../controllers/concerts.controller');
 
-let data = db.concerts;
+router.get('/concerts', ConcertsController.getAll);
+router.post('/concerts', ConcertsController.postConcert);
 
-router.route('/concerts')
-  .get((req, res) => res.json(data))
-  .post((req, res) => {
-    req.body.id = uuidv4();
-    const {performer, genre, price, day, image} = req.body;
-    if (performer && genre && price && day && image ) {
-      data.push(req.body)
-      res.json({ message: 'OK' });
-    } else res.json({message: 'Error'});
-  });
-
-router.route('/concerts/:id')
-  .get((req, res) => res.json(data.find(item => item.id.toString() === req.params.id)))
-  .put((req,res) => {
-    const {performer, genre, price, day, image} = req.body;
-    if (performer && genre && price && day && image ) {
-      data = data.map(item => {
-        if(item.id.toString() === req.params.id) {
-          item.performer = performer;
-          item.genre = genre;
-          item.price = price;
-          item.day = day;
-          item.image = image;
-        }
-        return item;
-      });
-      res.json({ message: 'OK' });
-    } else res.json({message: 'Error'});
-  })
-  .delete((req,res) => {
-    data = data.filter(item => item.id.toString() !== req.params.id);
-    res.json({ message: 'OK' });
-  });
+router.get('/concerts/:id', ConcertsController.getById);
+router.put('/concerts/:id', ConcertsController.putById);
+router.delete('/concerts/:id', ConcertsController.deleteById);
 
 module.exports = router;
