c6e3dc338d3fb03675760976fba027008aa5d002
Marcin Lech <arcinech@gmail.com>
2022-07-23 17:14:16 +0200

    Fix: server socket io cors emit

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Seat = require('./models/seat.model');
 const seatsRouts = require('./routes/seats.routes');
 const testimionalsRouts = require('./routes/testimionals.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const socket = require('socket.io');
-const db = require('./db');
+
+// const db = require('./db');
 
 const app = express();
 // Port for listening of the server 
 const serv =  app.listen(process.env.PORT || 8000);
 
-const origin = process.env.NODE_ENV === 'production' ? '*' : 'http://localhost:8000';
-// Socket for listening of the server
+// Socket for listening of the server -  cors origin set to any
 const io = socket(serv, {
   cors: {
-    origin: origin,
+    origin: '*',
   }
 });
 
@@ -24,20 +27,9 @@ app.use((req, res, next) => {
   next();
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit('seatsUpdated', db.seats);
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-
-
-
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
 
@@ -49,6 +41,29 @@ app.use('/api', concertsRoutes);
 // Middleware for client on production
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+db.on('error', err => console.log('Error ' + err));
+
+io.on('connection', async (socket) => {
+  try { 
+    console.log('a user connected');
+    const seats = await Seat.find({}).lean();
+    await socket.emit('seatsUpdated', seats);
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+  });
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
 
 app.use((req, res) => {
